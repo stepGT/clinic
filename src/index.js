d6417ec82962eb4c867c5118183bcd63400be8f7
Andrey <stepgt@mail.ru>
2020-03-25 16:31:09 +1000

    Added mobile burger menu

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ import './assets/styles/style.scss'
 import './bootstrap/bootstrap'
 
 $(document).ready(function ($) {
+  function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu__button');
+    let links = menu.find('.burger-menu__link');
+    let overlay = menu.find('.burger-menu__overlay');
 
-  $('form button').bind('click', function (e) {
-    e.preventDefault();
-  })
+    button.on('click', (e) => {
+      e.preventDefault();
+      toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+      menu.toggleClass('burger-menu_active');
+      if (menu.hasClass('burger-menu_active')) {
+        $('body').css('overflow', 'hidden');
+      }
+      else {
+        $('body').css('overflow', 'visible');
+      }
+    }
+  }
+  //
+  burgerMenu('.burger-menu');
 });
