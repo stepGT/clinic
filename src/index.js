cae2fab3d98324b65009d569f90f99c9a017c4d8
stepGT <stepgt@mail.ru>
2020-03-25 23:23:51 +1000

    Else refactor JS)

--- src/index.js
+++ src/index.js
@@ -7,20 +7,12 @@ $(document).ready(function ($) {
    */
   (function (selector) {
     let menu = $(selector);
-    let body = $('body');
-    let close = menu.find('.burger-menu__nav_back_close');
     //
     menu.find('.burger-menu__button').on('click', (e) => {
       e.preventDefault();
       toggleMenu();
     });
-    close.on('click', () => toggleMenu());
-    /**
-     *
-     */
-    const toggleMenu = () => {
-      menu.toggleClass('burger-menu_active');
-      menu.hasClass('burger-menu_active') ? body.css('overflow', 'hidden') : body.css('overflow', 'visible');
-    }
+    menu.find('.burger-menu__nav_back_close').on('click', () => toggleMenu());
+    const toggleMenu = () => menu.toggleClass('burger-menu_active');
   }('.burger-menu'));
 });
